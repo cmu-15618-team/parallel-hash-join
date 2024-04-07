4735ca41db412c788d2aaf9160bdb9243596350e
Gun9niR <gun9nir.guo@gmail.com>
2024-04-07 10:13:29 -0400

    feat: add join benchmark trait

---
+++ src/join_benchmark.rs
@@ -0,0 +1,38 @@
+use std::hint::black_box;
+
+fn time_phase(name: &str, f: impl FnOnce()) {
+    let start = std::time::Instant::now();
+    f();
+    let elapsed = start.elapsed();
+    println!("{}: {:?}", name, elapsed);
+}
+
+pub trait JoinBenchmark {
+    fn partition_phase(&self);
+    fn build_phase(&self);
+    fn probe_phase(&self);
+
+    fn run(&self) {
+        time_phase("partition", self.partition_phase());
+        time_phase("build", self.build_phase());
+        time_phase("probe", self.probe_phase());
+    }
+
+    /// Waste some time to simulate outputting a tuple.
+    /// See https://doc.rust-lang.org/std/hint/fn.black_box.html
+    fn produce_tuple() {
+        fn contains(haystack: &[&str], needle: &str) -> bool {
+            haystack.iter().any(|x| x == &needle)
+        }
+
+        fn waste_time() {
+            let haystack = vec!["candlelight"];
+            let needle = "candlelight";
+            for _ in 0..10 {
+                black_box(contains(black_box(&haystack), black_box(needle)));
+            }
+        }
+
+        black_box(waste_time());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+pub mod join_benchmark;
 pub mod tuple;
